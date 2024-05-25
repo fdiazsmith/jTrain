6bbfa05d99c7468f77be810a09d07850cd4922a7
Fernando <fdiazsmith@gmail.com>
2024-05-24 17:30:39 -0700

    chore: Update UnixTime class to fetch time from API and include timestamp calculations

--- UnixTime.cpp
+++ UnixTime.cpp
@@ -1,7 +1,59 @@
 #include "UnixTime.h"
+#include <ArduinoJson.h>
 
-UnixTime::UnixTime(long timestamp) {
-    this->timestamp = timestamp / 1000; // Convert to seconds
+UnixTime::UnixTime() : timestamp(0) {}
+
+UnixTime::UnixTime(int64_t timestamp) {
+    this->timestamp = timestamp/1000;
+}
+
+UnixTime::UnixTime(const String& continent, const String& city) {
+    if (!fetchTimeFromAPI(continent, city)) {
+        this->timestamp = 0;
+    }
+}
+
+bool UnixTime::fetchTimeFromAPI(const String& continent, const String& city) {
+    String url = "https://worldtimeapi.org/api/timezone/" + continent + "/" + city;
+    HTTPClient http;
+    http.begin(url);
+    int httpCode = http.GET();
+
+    if (httpCode > 0) {
+        if (httpCode == HTTP_CODE_OK) {
+            String payload = http.getString();
+            DynamicJsonDocument doc(1024);
+            DeserializationError error = deserializeJson(doc, payload);
+
+            if (!error) {
+                this->timestamp = doc["unixtime"].as<int64_t>();
+                this->dstoffset = doc["dst_offset"].as<long>();
+                this->rawOffset = doc["raw_offset"].as<long>() + this->dstoffset;
+                this->timestamp += this->rawOffset;
+                return true;
+            } else {
+                Serial.print("Deserialization error: ");
+                Serial.println(error.c_str());
+            }
+        } else {
+            Serial.print("HTTP error: ");
+            Serial.println(http.errorToString(httpCode).c_str());
+        }
+    } else {
+        Serial.print("Connection error: ");
+        Serial.println(http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
+    return false;
+}
+
+void UnixTime::setTimestamp(int64_t timestamp) {
+    this->timestamp = timestamp;
+}
+
+int64_t UnixTime::getTimestamp() const {
+    return timestamp;
 }
 
 // Helper function to check for leap year
@@ -18,8 +70,8 @@ int getDaysInMonth(int year, int month) {
     return daysInMonth[month - 1];
 }
 
-int UnixTime::getYear() {
-    long seconds = timestamp;
+int UnixTime::getYear() const {
+    int64_t seconds = timestamp;
     int year = 1970;
     while (seconds >= (isLeapYear(year) ? 366 : 365) * 24 * 3600) {
         seconds -= (isLeapYear(year) ? 366 : 365) * 24 * 3600;
@@ -28,8 +80,8 @@ int UnixTime::getYear() {
     return year;
 }
 
-int UnixTime::getMonth() {
-    long seconds = timestamp;
+int UnixTime::getMonth() const {
+    int64_t seconds = timestamp;
     int year = getYear();
     int month = 1;
     for (int i = 1970; i < year; i++) {
@@ -42,8 +94,8 @@ int UnixTime::getMonth() {
     return month;
 }
 
-int UnixTime::getDay() {
-    long seconds = timestamp;
+int UnixTime::getDay() const {
+    int64_t seconds = timestamp;
     int year = getYear();
     int month = getMonth();
     for (int i = 1970; i < year; i++) {
@@ -55,23 +107,31 @@ int UnixTime::getDay() {
     return seconds / (24 * 3600) + 1;
 }
 
-int UnixTime::getHour() {
+int UnixTime::getHour() const {
     return (timestamp % 86400L) / 3600;
 }
 
-int UnixTime::getMinute() {
+int UnixTime::getMinute() const {
     return (timestamp % 3600) / 60;
 }
 
-int UnixTime::getSecond() {
+int UnixTime::getSecond() const {
     return timestamp % 60;
 }
 
-void UnixTime::getDateTimeComponents(int &year, int &month, int &day, int &hour, int &minute, int &second) {
+void UnixTime::getDateTimeComponents(int &year, int &month, int &day, int &hour, int &minute, int &second) const {
     year = getYear();
     month = getMonth();
     day = getDay();
     hour = getHour();
     minute = getMinute();
     second = getSecond();
+}
+
+UnixTime UnixTime::operator-(const UnixTime& other) const {
+    return UnixTime(this->timestamp - other.timestamp);
+}
+
+void UnixTime::update() {
+    this->timestamp += millis() / 1000;
 }
--- UnixTime.h
+++ UnixTime.h
@@ -2,22 +2,38 @@
 #define UNIXTIME_H
 
 #include <Arduino.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
 
 class UnixTime {
 private:
-    long timestamp; // Unix timestamp in seconds
+    int64_t timestamp; // Unix timestamp in seconds
+    long dstoffset;
+    long rawOffset;
+
+    bool fetchTimeFromAPI(const String& continent, const String& city);
 
 public:
-    UnixTime(long timestamp);
+    UnixTime();
+    UnixTime(int64_t timestamp);
+    UnixTime(const String& continent, const String& city);
 
-    int getYear();
-    int getMonth();
-    int getDay();
-    int getHour();
-    int getMinute();
-    int getSecond();
+    void setTimestamp(int64_t timestamp);
+    int64_t getTimestamp() const;
 
-    void getDateTimeComponents(int &year, int &month, int &day, int &hour, int &minute, int &second);
+    int getYear() const;
+    int getMonth() const;
+    int getDay() const;
+    int getHour() const;
+    int getMinute() const;
+    int getSecond() const;
+    void setRawOffset(long offset) { rawOffset = offset; timestamp += offset;}
+    long getRawOffset() const { return rawOffset; }
+    void getDateTimeComponents(int &year, int &month, int &day, int &hour, int &minute, int &second) const;
+
+    UnixTime operator-(const UnixTime& other) const;
+
+    void update();
 };
 
 #endif
